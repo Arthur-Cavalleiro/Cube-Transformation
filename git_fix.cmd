2d9a8aee10640ef56919212f9d620f1413604e80
Arthur Cavalleiro <98990206+Arthur-Cavalleiro@users.noreply.github.com>
2025-03-30 19:38:19 -0300

    Delete git_fix.cmd

--- git_fix.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Fixing unrelated histories issue...
-git pull --allow-unrelated-histories https://github.com/Arthur-Cavalleiro/Cube-Transformation main
-
-echo.
-echo If you encounter merge conflicts, resolve them and then run:
-echo git add .
-echo git commit -m "Merge remote repository"
-echo git push
